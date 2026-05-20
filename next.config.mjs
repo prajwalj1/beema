8cf82893094e61270e422bcdb00d273ef963dd06
Prajwal Gautam <prajwal.gautam2727@gmail.com>
2026-05-20 19:09:33 +0545

    fix: resolve login redirects and npm conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXTAUTH_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.NEXTAUTH_URL || 'http://localhost:3000',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "fallback_production_secret_do_not_use_in_real_app"
+  }
 };
 
 export default nextConfig;
